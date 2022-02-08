3ccaeb4553788f957345fd8ffa643a234e16c477
nobilissimum <ronn.angelo.lee@gmail.com>
2022-02-08 17:47:17 +0800

    :zap: Navigation for mobile view

--- src/components/layout/components/navbar.js
+++ src/components/layout/components/navbar.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import IconComponent from 'components/utils/icon';
+import { useState } from 'react';
 
-const NavbarComponent = () => {
-  return <div>Navbar</div>;
+const ToolbarStyledComponent = styled(Toolbar)(({ theme }) => ({
+  paddingLeft: theme.spacing(2),
+  paddingRight: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  [theme.breakpoints.up('md')]: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
+
+const AppBarStyledComponent = styled(AppBar, {
+  shouldForwardProp: (prop) =>
+    prop !== 'drawerOpen' && prop !== 'noHover' && prop !== 'onTop',
+})(({ drawerOpen, noHover, onTop, theme }) => ({
+  background: theme.palette.background.paper,
+  color: theme.palette.text.primary,
+  boxShadow: theme.shadows[0],
+  marginLeft: theme.spacing(0),
+  width: `calc(100% - ${theme.spacing(0)})`,
+  ...(noHover && {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(7),
+    width: `calc(100% - ${theme.spacing(7)})`,
+  },
+  [theme.breakpoints.up('md')]: {
+    marginLeft: theme.spacing(34),
+    width: `calc(100% - ${theme.spacing(34)})`,
+  },
+  ...(drawerOpen && {
+    ...(noHover && {
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(30),
+      width: `calc(100% - ${theme.spacing(30)})`,
+    },
+    [theme.breakpoints.up('md')]: {
+      marginLeft: theme.spacing(9),
+      width: `calc(100% - ${theme.spacing(9)})`,
+    },
+  }),
+  ...(!onTop && {
+    outline: `1px solid ${theme.palette.divider}`,
+  }),
+}));
+
+const NavbarComponent = ({ drawerOpen, handleDrawerOpen, noHover }) => {
+  // States
+  const [onTop, setOnTop] = useState(true);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 25) setOnTop(false);
+    else setOnTop(true);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  return (
+    <AppBarStyledComponent
+      position='fixed'
+      drawerOpen={drawerOpen}
+      noHover={noHover}
+      onTop={onTop}
+    >
+      <ToolbarStyledComponent sx={{ px: 1 }}>
+        <IconButton
+          onClick={handleDrawerOpen}
+          sx={{ display: { xs: 'flex', sm: 'none' } }}
+        >
+          <IconComponent icon='list' iconType='phosphor' weight='regular' />
+        </IconButton>
+        <Box noWrap sx={{ flexGrow: 1 }} component='div' />
+      </ToolbarStyledComponent>
+    </AppBarStyledComponent>
+  );
 };
 
 export default NavbarComponent;
