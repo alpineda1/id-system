8b634c99087d0e4aa763913514f9d17ed0f9ee1b
nobilissimum <ronn.angelo.lee@gmail.com>
2022-02-11 21:13:44 +0800

    :zap: Change color theme to fit NU-APC

--- src/modules/theme/components/dark.js
+++ src/modules/theme/components/dark.js
@@ -1,5 +1,16 @@
 const DarkTheme = {
-  palette: {},
+  palette: {
+    primary: {
+      main: '#41509C',
+      light: '#5161AD',
+      dark: '#35438c',
+    },
+    secondary: {
+      main: '#F5C056',
+      light: '#FAC966',
+      dark: '#E7AF41',
+    },
+  },
 };
 
 export default DarkTheme;
--- src/modules/theme/components/light.js
+++ src/modules/theme/components/light.js
@@ -1,5 +1,16 @@
 const LightTheme = {
-  palette: {},
+  palette: {
+    primary: {
+      main: '#35438c',
+      light: '#414F96',
+      dark: '#2F3B75',
+    },
+    secondary: {
+      main: '#E7AF41',
+      light: '#F5C056',
+      dark: '#CFA044',
+    },
+  },
 };
 
 export default LightTheme;
