ebd7bbb5527a5acb02ecb1ffabe183158191f489
alpineda1 <alpineda@student.apc.edu.ph>
2022-02-23 15:47:54 +0800

    search

--- src/components/history-list/index.js
+++ src/components/history-list/index.js
@@ -1,58 +1,30 @@
 import React from "react"
-import { useState, useEffect} from "react";
-import { Button, Typography , Stack} from '@mui/material';
-import {db} from 'firebase.app'
+//import { useState, useEffect} from "react";
+import { Button, Typography , Stack, TextField} from '@mui/material';
+//import {db} from 'firebase.app'
 //import FilterResults from 'react-filter-search';
 //import SearchInput, {createFilter} from 'react-search-input'
 //import { SearchIcon, CloseIcon} from "@mui/material"
 
 const HistoryList = () => {
 
-  const [search, setSearch] = useState([]);
-  const [name, SetName] = useState([]);
-  const[NameData, setNameData] = useState(name);
-
-  const changeSearch = (val) => {
-    setSearch(val)
-    if(val !=' ') {
-      setNameData(name.filter(  name => {
-        name.first.includes (val) ||
-        name.last.includes(val)
-        
-      }))
-    }
-    else{
-      setNameData(name)
-    }
-  }
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await db.collection ("name").get();
-      SetName(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
-
-        };
-        fetchData();
-      }, []);
-
-
     return (
     <Stack spacing={2} >
       <Stack spacing={2}>
+      <Stack spacing={2}>
            <Typography variant='h6' align="center">ID  Histroy List</Typography>
       </Stack>
-          {/*  <Typography >Search: <input type="text" value={search} onChange={handleSearchChange} /></Typography>
-     
-      <Button value={data.idNumber}></Button>
 
-      
-     */}
-<input type='text' onChange={(e)=> changeSearch(e.target.value)}></input>
-<input
-          type="text"
-          placeholder="Search"
-          onChange={(e) => setSearch(e.target.value)}
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Search"
         />
-    <Button  variant="contained" >sample</Button>
+        </div>
+        </Stack>
+      <Button  variant="contained" >sample</Button>
           </Stack>
          
   ); 
