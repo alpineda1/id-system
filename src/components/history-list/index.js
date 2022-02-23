55ceef75d499c4378cfc598b26ccf4c8a7bd244f
alpineda1 <alpineda@student.apc.edu.ph>
2022-02-23 22:04:13 +0800

    test

--- src/components/history-list/index.js
+++ src/components/history-list/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 //import { useState, useEffect} from "react";
-import { Button, Typography , Stack, TextField} from '@mui/material';
+import { Button, Typography , Stack, TextField,Card,ButtonGroup} from '@mui/material';
 //import {db} from 'firebase.app'
 //import FilterResults from 'react-filter-search';
 //import SearchInput, {createFilter} from 'react-search-input'
@@ -10,6 +10,7 @@ const HistoryList = () => {
 
     return (
     <Stack spacing={2} >
+      <Stack spacing={2}>
       <Stack spacing={2}>
       <Stack spacing={2}>
            <Typography variant='h6' align="center">ID  Histroy List</Typography>
@@ -26,8 +27,22 @@ const HistoryList = () => {
           justify="flex-end"
         />
         </div>
+</Stack>
+<Card>
+  <Typography variant="subtitle1">Sort By</Typography>
+<ButtonGroup variant="contained" aria-label="outlined primary button group">
+  <Button>Today</Button>
+  <Button>Week</Button>
+  <Button>Month</Button>
+</ButtonGroup>
+        </Card>
         </Stack>
-      <Button  variant="contained" >Pineda, Angelika L || 2018-100263</Button>
+      <Button  variant="contained" >Delacerna, Jerome D || 2018-100003</Button>
+      <Button  variant="contained" >Pineda, Angelika L || 2018-100100</Button>
+      <Button  variant="contained" >Rianzares, Kervin R || 2018-100242</Button>
+      <Button  variant="contained" >Sarte, Neil Y || 2018-100241</Button>
+      <Button  variant="contained" >Veluz, Sage S || 2018-100263</Button>
+      <Button  variant="contained" >Ylagan, Lala L || 2019-100300</Button>
           </Stack>
          
   ); 
