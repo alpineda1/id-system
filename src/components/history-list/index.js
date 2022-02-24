9279079bb2ebaabce94dbeceffa330ae35881e7d
alpineda1 <alpineda@student.apc.edu.ph>
2022-02-24 15:21:03 +0800

    search bar

--- src/components/history-list/index.js
+++ src/components/history-list/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 //import { useState, useEffect} from "react";
-import { Button, Typography , Stack, TextField,Divider} from '@mui/material';
+import { Button, Typography , Stack, TextField} from '@mui/material';
 //import {db} from 'firebase.app'
 //import FilterResults from 'react-filter-search';
 //import SearchInput, {createFilter} from 'react-search-input'
@@ -21,18 +21,15 @@ const HistoryList = () => {
         <TextField
           id="outlined-basic"
           variant="outlined"
-          fullWidth
           label="Search"
           direction="row"
           alignItems = "center"
           justify="flex-end"
-          style={{ height: 28 , width: "80%"}}
+          style={{ height: 10 , width: "80%"}}
         />
         </div>
 </Stack>
 
-
-<Divider/>
       <Button  variant="contained"  style={{ width: "100%" }} >Dela Cerna, Jerome D || 2018-100003</Button>
       <Button  variant="contained"  style={{ width: "100%" }}>Pineda, Angelika L || 2018-100100</Button>
       <Button  variant="contained"  style={{ width: "100%" }}>Rianzares, Kervin R || 2018-100242</Button>
