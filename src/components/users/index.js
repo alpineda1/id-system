da66b0aca0ca136e177fb8fb17c46379285cc369
nobilissimum <ronn.angelo.lee@gmail.com>
2022-03-01 10:30:58 +0800

    :zap: Remove unused vars

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const UsersComponent = () => {
-  const [data, setData] = useState();
+  // const [data, setData] = useState();
 
   const isMounted = useRef(true);
 
